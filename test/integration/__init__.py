97e8feef811bf5b744f416e1f427b094d0ee1184
Ying Wang <yingw787.personal@gmail.com>
2020-03-02 17:12:40 -0500

    Add test/ stubbing out what types of tests to add for TinyDevCRM API. Building as a separate package in order to make testing a first-class citizen, and to facilitate stripping out tests for production builds.

---
+++ test/integration/__init__.py
@@ -0,0 +1,7 @@
+"""
+Integration tests for the TinyDevCRM API.
+
+Ideally, integration tests should be a combination of contract testing and
+property-based testing, to avoid unnecessary usage of oracle values and better
+enable parallelization: https://hillelwayne.com/post/pbt-contracts/
+"""
---
+++ test/pbt/__init__.py
@@ -0,0 +1,3 @@
+"""
+Property-based tests for the TinyDevCRM API.
+"""
