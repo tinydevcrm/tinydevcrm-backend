469dbab5fa8266da1a0d99128328dfab265e5757
Ying Wang <yingw787.personal@gmail.com>
2020-03-03 17:28:26 -0500

    Update authentication/urls.py update paths to include trailing slash, otherwise route will 404.

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -9,12 +9,12 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     # Override simplejwt stock token
     path(
-        'token/obtain',
+        'token/obtain/',
         jwt_views.TokenObtainPairView.as_view(),
         name='token_create'
     ),
     path(
-        'token/refresh',
+        'token/refresh/',
         jwt_views.TokenRefreshView.as_view(),
         name='token_refresh'
     )
