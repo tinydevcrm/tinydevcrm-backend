45f94b11c98416164ed72d8c40e9ac2a1e40f41e
Ying Wang <yingw787.personal@gmail.com>
2020-03-03 18:05:58 -0500

    Update authentication/views.py adding custom view for custom serializer.

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import MyTokenObtainPairSerializer
+
 
 # Create your views here.
+class ObtainTokenPairWithColorView(TokenObtainPairView):
+    # NOTE: Last comma is important, otherwise an iteration error will result
+    # because wrapping a str with parens results in str while wrapping str +
+    # comma with parens results in tuple
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
