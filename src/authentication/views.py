4b8d04de5af0481db9c4bf0e6b5d843f4d28032e
Ying Wang <yingw787.personal@gmail.com>
2020-03-04 13:07:28 -0500

    Update views.py create CustomUserCreate APIView.

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import MyTokenObtainPairSerializer
+from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 
 
 # Create your views here.
 class ObtainTokenPairWithColorView(TokenObtainPairView):
-    # NOTE: Last comma is important, otherwise an iteration error will result
-    # because wrapping a str with parens results in str while wrapping str +
-    # comma with parens results in tuple
-    permission_classes = (permissions.AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+
+class CustomUserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(
+                    json,
+                    status=status.HTTP_201_CREATED
+                )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
