a7eb0838a33de03c184809bb1c3bca6ae358a228
Ying Wang <yingw787.personal@gmail.com>
2020-03-04 13:07:09 -0500

    Update serializers.py add CustomUserSerializer.

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -2,7 +2,10 @@
 Django REST Framework custom serializers.
 """
 
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .models import CustomUser
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -13,3 +16,29 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['fav_color'] = user.fav_color
 
         return token
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    """
+    Currently unused in preference of the below.
+    """
+    email = serializers.EmailField(
+        required=True
+    )
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        # As long as the fields are the same, we can just use this
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
