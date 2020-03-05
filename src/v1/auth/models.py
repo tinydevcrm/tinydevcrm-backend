14659d86ca4907188c8f54b17d2f2718a2bd4fb3
Ying Wang <yingw787.personal@gmail.com>
2020-03-05 17:05:42 -0500

    Update models.py creating CustomUser.

--- src/v1/auth/models.py
+++ src/v1/auth/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+"""
+V1 authentication API models registration.
+"""
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    """
+    Customized user for JWT-based authentication.
+
+    Ideally, this user can be extended in order to provide JWT-based API token
+    scoping for various database and backend resources.
+    """
