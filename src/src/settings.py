8e6fa971aa03ed29f1e6023134f37dd32f004677
Ying Wang <yingw787.personal@gmail.com>
2020-03-05 12:48:19 -0500

    Update settings.py BLACKLIST_AFTER_ROTATION from False to True.

--- src/src/settings.py
+++ src/src/settings.py
@@ -154,7 +154,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
