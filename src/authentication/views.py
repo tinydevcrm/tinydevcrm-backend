0234e179c4e8b226769fe8e27eb0c41e5d1c74b0
Ying Wang <yingw787.personal@gmail.com>
2020-03-05 12:54:30 -0500

    Update views.py adding view to logout and blacklist refresh token for user views.

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -3,6 +3,7 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 
@@ -38,3 +39,21 @@ class HelloWorldView(APIView):
             data={'hello': 'world'},
             status=status.HTTP_200_OK
         )
+
+
+class LogoutAndBlacklistRefreshTokenForUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh_token']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(
+                status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST
+            )
