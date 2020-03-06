03f973225d4e6cbb494749ab35729c92ab1a1d42
Ying Wang <yingw787.personal@gmail.com>
2020-03-05 19:57:47 -0500

    Update models.py adding CustomUserManager model, tried to login to Django admin, and getting redirected for some reason.

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,5 +1,50 @@
+"""
+Authentication service models.
+"""
+
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
+
+
+class CustomManager(BaseUserManager):
+    """
+    Customized manager for managing CustomUser models and accessing the Django
+    admin dashboard; the dashboard breaks otherwise. This is necessary as the
+    primary_email for a user is the unique identifier for authentication instead
+    of a username. See: https://stackoverflow.com/a/16530435/1497211 and
+    https://docs.djangoproject.com/en/dev/topics/auth/customizing/
+
+    This logic is mostly taken from this tutorial:
+    https://testdriven.io/blog/django-custom-user-model/#abstractuser-vs-abstractbaseuser
+    """
+    def create_user(self, email, password, **kwargs):
+        """
+        Create and save an instance of CustomUser with the given email and
+        password.
+        """
+        if not email:
+            raise ValueError(_('Email is required.'))
+        email = self.normalize_email(email)
+        user = self.model(primary_email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        """
+        Create and save an instance of CustomUser with superuser privileges.
+        """
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+
+        if not kwargs.get('is_staff'):
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if not kwargs.get('is_superuser'):
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **kwargs)
 
 
 # Create your models here.
@@ -14,6 +59,8 @@ class CustomUser(AbstractBaseUser):
     resets. Passwords should also be salted by default:
     https://docs.djangoproject.com/en/3.0/topics/auth/passwords/
     """
+    objects = CustomManager()
+
     # Keep the full name around as a Unicode-encoded bytearray:
     # https://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/
     #
