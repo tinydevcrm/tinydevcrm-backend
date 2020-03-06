ae6bb9d861a38790a272ad16dc2fed6059fc2fc9
Ying Wang <yingw787.personal@gmail.com>
2020-03-05 19:42:56 -0500

    Update urls.py views.py commenting out functionality that I don't necessarily need yet.

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -5,15 +5,14 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
+from .serializers import CustomTokenObtainPairSerializer, CustomUserSerializer
 
 
-# Create your views here.
-class ObtainTokenPairWithColorView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+# class ObtainTokenPairWithColorView(TokenObtainPairView):
+#     serializer_class = CustomTokenObtainPairSerializer
 
 
-class CustomUserCreate(APIView):
+class CustomUserRegister(APIView):
     permission_classes = (permissions.AllowAny,)
     authentication_classes = ()
 
@@ -33,27 +32,27 @@ class CustomUserCreate(APIView):
         )
 
 
-class HelloWorldView(APIView):
-    def get(self, request):
-        return Response(
-            data={'hello': 'world'},
-            status=status.HTTP_200_OK
-        )
+# class HelloWorldView(APIView):
+#     def get(self, request):
+#         return Response(
+#             data={'hello': 'world'},
+#             status=status.HTTP_200_OK
+#         )
 
 
-class LogoutAndBlacklistRefreshTokenForUserView(APIView):
-    permission_classes = (permissions.AllowAny,)
-    authentication_classes = ()
+# class LogoutAndBlacklistRefreshTokenForUserView(APIView):
+#     permission_classes = (permissions.AllowAny,)
+#     authentication_classes = ()
 
-    def post(self, request):
-        try:
-            refresh_token = request.data['refresh_token']
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(
-                status=status.HTTP_205_RESET_CONTENT
-            )
-        except Exception as e:
-            return Response(
-                status=status.HTTP_400_BAD_REQUEST
-            )
+#     def post(self, request):
+#         try:
+#             refresh_token = request.data['refresh_token']
+#             token = RefreshToken(refresh_token)
+#             token.blacklist()
+#             return Response(
+#                 status=status.HTTP_205_RESET_CONTENT
+#             )
+#         except Exception as e:
+#             return Response(
+#                 status=status.HTTP_400_BAD_REQUEST
+#             )
