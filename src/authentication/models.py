bcae0d426eb49c68c72dc5fdc0f7a903e16bc3df
Ying Wang <yingw787.personal@gmail.com>
2020-03-06 14:42:40 -0500

    Update models.py working custom users, might need to issue an additional save.

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -19,7 +19,7 @@ class CustomUserManager(BaseUserManager):
     This logic is mostly taken from this tutorial:
     https://testdriven.io/blog/django-custom-user-model/#abstractuser-vs-abstractbaseuser
     """
-    def create_user(self, primary_email, full_name, password, is_staff=False, **kwargs):
+    def create_user(self, primary_email, password, full_name, **kwargs):
         """
         Create and save an instance of CustomUser with the given primary_email
         and password.
@@ -27,7 +27,11 @@ class CustomUserManager(BaseUserManager):
         if not primary_email:
             raise ValueError('Email is required.')
         primary_email = self.normalize_email(primary_email)
-        user = self.model(primary_email=primary_email, password=password, full_name=full_name)
+        user = self.model(
+            primary_email=primary_email,
+            password=password,
+            full_name=full_name
+        )
         user.set_password(password)
         user.save()
 
@@ -37,15 +41,23 @@ class CustomUserManager(BaseUserManager):
         """
         Create and save an instance of CustomUser with superuser privileges.
         """
-        kwargs.setdefault('is_staff', True)
-        kwargs.setdefault('is_superuser', True)
-        kwargs.setdefault('is_active', True)
+        new_user = self.create_user(
+            primary_email,
+            password,
+            full_name,
+            **kwargs
+        )
 
-        if not kwargs.get('is_staff'):
-            raise ValueError('Superuser must have is_staff=True.')
-        if not kwargs.get('is_superuser'):
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(primary_email, password, full_name, **kwargs)
+        # NOTE: Might have to issue an additional save with superuser
+        # properties. Ideally, these properties can be set in kwargs, and passed
+        # to method 'create_user()', which will handle saves at time of user
+        # creation.
+        new_user.is_staff = True
+        new_user.is_active = True
+        new_user.is_superuser = True
+        new_user.save()
+
+        return new_user
 
 
 # Create your models here.
@@ -69,8 +81,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     # https://stackoverflow.com/a/2597994/1497211
     full_name = models.CharField(max_length=255)
 
-    # Use email as the primary username, since emails address syntax is defined
-    # in IEEE RFC-5322: https://tools.ietf.org/html/rfc5322
+    # Use field 'email' as the primary username, since emails address syntax is
+    # defined in IEEE RFC-5322: https://tools.ietf.org/html/rfc5322, and because
+    # framework likely will not require users to identify each other. If
+    # additional emails are registered, use 'secondary_emails' or similar to
+    # handle
     primary_email = models.EmailField(unique=True)
 
     # Default permission levels.
@@ -79,4 +94,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'primary_email'
-    REQUIRED_FIELDS = []
+    # NOTE: I'm not sure whether it's a good idea to make a full name required.
+    # Intended usage at the moment is just as a tag, while the email address
+    # should be carried around as the only unique constraint. Having too many
+    # unique constraints may make the data model brittle down the line.
+    REQUIRED_FIELDS = ['full_name']
