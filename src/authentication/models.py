032165d2988413c68948070d14b587ecf6cd8078
Ying Wang <yingw787.personal@gmail.com>
2020-03-06 14:14:43 -0500

    Update models.py committing broken model.

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -4,10 +4,11 @@ Authentication service models.
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 
-class CustomManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
     """
     Customized manager for managing CustomUser models and accessing the Django
     admin dashboard; the dashboard breaks otherwise. This is necessary as the
@@ -18,21 +19,21 @@ class CustomManager(BaseUserManager):
     This logic is mostly taken from this tutorial:
     https://testdriven.io/blog/django-custom-user-model/#abstractuser-vs-abstractbaseuser
     """
-    def create_user(self, email, password, **kwargs):
+    def create_user(self, primary_email, full_name, password, is_staff=False, **kwargs):
         """
-        Create and save an instance of CustomUser with the given email and
-        password.
+        Create and save an instance of CustomUser with the given primary_email
+        and password.
         """
-        if not email:
-            raise ValueError(_('Email is required.'))
-        email = self.normalize_email(email)
-        user = self.model(primary_email=email, **kwargs)
+        if not primary_email:
+            raise ValueError('Email is required.')
+        primary_email = self.normalize_email(primary_email)
+        user = self.model(primary_email=primary_email, password=password, full_name=full_name)
         user.set_password(password)
         user.save()
 
         return user
 
-    def create_superuser(self, email, password, **kwargs):
+    def create_superuser(self, primary_email, password, full_name, **kwargs):
         """
         Create and save an instance of CustomUser with superuser privileges.
         """
@@ -41,14 +42,14 @@ class CustomManager(BaseUserManager):
         kwargs.setdefault('is_active', True)
 
         if not kwargs.get('is_staff'):
-            raise ValueError(_('Superuser must have is_staff=True.'))
+            raise ValueError('Superuser must have is_staff=True.')
         if not kwargs.get('is_superuser'):
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **kwargs)
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(primary_email, password, full_name, **kwargs)
 
 
 # Create your models here.
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
     Customized user for JWT-based authentication.
 
@@ -59,7 +60,7 @@ class CustomUser(AbstractBaseUser):
     resets. Passwords should also be salted by default:
     https://docs.djangoproject.com/en/3.0/topics/auth/passwords/
     """
-    objects = CustomManager()
+    objects = CustomUserManager()
 
     # Keep the full name around as a Unicode-encoded bytearray:
     # https://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/
@@ -72,4 +73,10 @@ class CustomUser(AbstractBaseUser):
     # in IEEE RFC-5322: https://tools.ietf.org/html/rfc5322
     primary_email = models.EmailField(unique=True)
 
+    # Default permission levels.
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'primary_email'
+    REQUIRED_FIELDS = []
