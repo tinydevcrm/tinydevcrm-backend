091282b10428b176e502ee74a5fc90d8f7967b2c
Ying Wang <yingw787.personal@gmail.com>
2020-03-09 17:43:35 -0400

    Renaming src/src to src/core in order to deconflict eb deployment settings as per stack overflow settings.

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,4 +1,4 @@
-"""src URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
