88fab5f085b934bb70c224c717712157dceec92c
Ying Wang <yingw787.personal@gmail.com>
2020-03-18 21:19:22 -0400

    Add ecs-deploy.sh.

---
+++ scripts/ecs-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Deploy API instance to Amazon Web Services (AWS) Elastic Container Service
+# (ECS) using `ecs-cli compose`:
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/cmd-ecs-cli-compose.html
