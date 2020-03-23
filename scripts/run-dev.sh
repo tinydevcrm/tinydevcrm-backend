9c66448a3782e5f5829d2626a52decd1a4fd9658
Ying Wang <yingw787.personal@gmail.com>
2020-03-23 17:35:47 -0400

    Update scripts/run-dev.sh.

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,3 +1,30 @@
 #!/usr/bin/env bash
 #
-# Run development environment.
+# Run development environment locally.
+#
+# Assumptions:
+#
+# The following dependencies are installed and available in $PATH:
+#
+# - `git` (I am using v2.25.2)
+# - `docker` (I am using v19.03.6, build 369ce74a3c)
+# - `docker-compose` (I am using v1.23.2, build 1110ad01)
+#
+# No other containers are currently running that assume ports taken up by
+# $GIT_REPO_ROOT/services/docker-compose.development.yaml.
+#
+# `docker volume ls` does not have service-specific dependencies.
+#
+# `docker network ls` does not have service-specific dependencies.
+#
+# If you're not sure, run `docker system prune -a` in order to clear all
+# `docker` context on local.
+
+# Print out commands to the terminal as they run.
+set -x
+
+GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+
+$(which docker-compose) \
+    -f $GIT_REPO_ROOT/services/docker-compose.development.yaml \
+    up -d --build
