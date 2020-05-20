6dcad4d7d2de4075d69ff8c1f2ae2e0acac68a2d
Ying Wang <yingw787.personal@gmail.com>
2020-05-20 15:56:49 -0400

    Update utils.py add one method.

--- services/app/src/concrete_tables/utils.py
+++ services/app/src/concrete_tables/utils.py
@@ -3,6 +3,11 @@ Utility classes, methods, and variables for concrete table service.
 """
 
 import enum
+import os
+
+from psycopg2 import sql
+
+from core import utils as core_utils
 
 
 class RecognizedPostgreSQLTypes(enum.Enum):
@@ -64,5 +69,53 @@ def create_table_data_is_valid(request_body):
             checks['column_keys_match'] = False
             # NOTE: Exiting early by choice, should not error out if no return.
             return all(checks.values())
+
+    return all(checks.values())
+
+
+def import_data_into_table_request_is_valid(file_path, table_name):
+    """
+    Validates whether request for API endpoint /v1/tables/import/ is valid.
+
+    Checks for:
+    - 'file_path' is a valid path to file.
+    - 'table_name' is a valid table.
+
+    Args:
+        str: file_path
+        str: table_name
+
+    Returns:
+        bool: Request is valid.
+    """
+    # TODO: Add check to ensure schemas between table and file match.
+
+    checks = {
+        'file_exists': True,
+        'table_exists': True
+    }
+
+    # Validate that the file exists.
+    if not os.path.exists(file_path):
+        checks['file_exists'] = False
+
+    # Validate that the table exists.
+    #
+    # TODO: Update logic to take into account user-specific schemas if
+    # necessary.
+    try:
+        psql_conn = core_utils.create_fresh_psql_connection()
+        psql_cursor = psql_conn.cursor()
+        sql_query = sql.SQL(
+            'SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_name={})'
+        ).format(sql.Literal(table_name))
+        psql_cursor.execute(sql_query)
+        table_exists = psql_cursor.fetchone()[0]
+        checks['table_exists'] = table_exists
+    except Exception as e:
+        checks['table_exists'] = False
+    finally:
+        psql_cursor.close()
+        psql_conn.close()
 
     return all(checks.values())
