e06fbfa83ae8439703b7539bdaf8e13099170122
Ying Wang <yingw787.personal@gmail.com>
2020-05-21 11:25:35 -0400

    Update __init__.py copying over concrete_data service into concrete_tables.

--- services/app/src/concrete_tables/__init__.py
+++ services/app/src/concrete_tables/__init__.py
@@ -1,13 +1,25 @@
 """
 Concrete tables service.
 
-This Django app pre-creates PostgreSQL tables for ingestion of "concrete data".
-This way, no expensive type inference needs to be run on the data, and usage of
-the native PostgreSQL COPY FROM tool can be leveraged in lieu of building a
-custom RFC-4180 compliant CSV parser.
+This Django app manages API endpoints related to managing PostgreSQL tables.
+These tables are foreign tables backed by "concrete data", Parquet files
+available via 'parquet_fdw' (foreign data wrapper) that serve as the
+foundational sources of truth for users. This is opposed to "derived data",
+which is data computed via mathematical, logical / relational, or other types of
+transformations. For example, a materialized view would be considered "derived
+data", while a CSV upload would be considered "concrete data".
+
+Making this distinction guarantees unitary application data flow, and that
+consequently, underlying data pipelines are acyclic. This methodology may reduce
+likelihood of data corruption via concurrency / paralellism or other concerns,
+and helps describe the data model more clearly.
 
 TODO: I'm not terribly sure how to take the complete possible PostgreSQL 'CREATE
 TABLE' syntax and translate that through a form. Passing raw SQL queries could
 result in security issues. At the same time, not having the ability to template
 out the SQL 'CREATE TABLE' syntax may result in much decreased functionality.
+
+TODO: Support table "forking" for alteration of concrete data.
+
+TODO: Create filesystem of users to "shard" foreign tables by user.
 """
