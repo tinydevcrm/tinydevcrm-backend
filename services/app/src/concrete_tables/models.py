26c62e5eb2d71b87f29c593111166368091c7956
Ying Wang <yingw787.personal@gmail.com>
2020-05-21 11:25:58 -0400

    Update models.py add model DataFile.

--- services/app/src/concrete_tables/models.py
+++ services/app/src/concrete_tables/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO: Create association between user and table, if sharding data files by
+# user is relevant.
+
+import os
+
+from django.conf import settings
+from django.db import models
+
+
+CONCRETE_TABLE_URL = 'concrete-tables'
+CONCRETE_TABLE_ROOT = os.path.join(
+    settings.MEDIA_ROOT,
+    CONCRETE_TABLE_URL
+)
+
+
+class DataFile(models.Model):
+    """
+    Model for data files uploaded to TinyDevCRM.
+    """
+    file_id = models.AutoField(primary_key=True)
+    file = models.FileField(
+        upload_to=CONCRETE_TABLE_URL,
+        blank=False,
+        null=False
+    )
+
+    def __str__(self):
+        return str(self.file.name)
