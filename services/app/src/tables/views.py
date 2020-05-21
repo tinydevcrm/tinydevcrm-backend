fc42fba7362e842efee1ea8cf4a3349f17480237
Ying Wang <yingw787.personal@gmail.com>
2020-05-21 12:37:21 -0400

    Add views.py working /v1/tables/create/.

--- services/app/src/tables/views.py
+++ services/app/src/tables/views.py
@@ -2,161 +2,24 @@
 Table service custom views.
 """
 
+import json
 import os
 
 import psycopg2
 from psycopg2 import sql
 from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from . import models
-from core import utils as core_utils
-
-from . import utils
-
-
-class CreateTableView(APIView):
-    """
-    Handles 'CREATE TABLE' views via API.
-    """
-    def post(self, request, *args, **kwargs):
-        """
-        Handles the HTTP POST request.
-
-        Example usage:
-
-        - curl \
-            --header "Content-Type: application/json" \
-            --header "Authorization: JWT $JWT_ACCESS_TOKEN" \
-            --method POST
-            --data '{"dry_run": "1", "name": "some_table", "columns": [{"name": "columnA", "type": "nvarchar(256)"}, {"name": "columnB", "type": "bytea"}]}'
-            https://api.tinydevcrm.com/v1/tables/create/
-        """
-        request_body = request.data
-
-        if not utils.create_table_data_is_valid(request_body):
-            return Response(
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        table_name = request_body.get('name')
-        columns = request_body.get('columns')
-        column_query = []
-        for column in columns:
-            column_query.append(
-                ' '.join([
-                    '"' + column.get('name') + '"',
-                    column.get('type').upper()
-                ])
-            )
-        column_query = ', '.join(column_query)
-        column_query = '(' + column_query + ')'
-
-        sql_query = f'CREATE TABLE "{table_name}" {column_query};'
-
-        dry_run = int(request_body.get('dry_run'))
-        if dry_run:
-            return Response(
-                {
-                    "sql_query": sql_query
-                },
-                status=status.HTTP_200_OK
-            )
-        else:
-            # NOTE: Wrap connection in try/except/finally block in order to
-            # responsibly handle possible errors in executing SQL query.
-            try:
-                psql_conn = core_utils.create_fresh_psql_connection()
-                psql_cursor = psql_conn.cursor()
-                psql_cursor.execute(
-                    sql.SQL(sql_query)
-                )
-                psql_conn.commit()
-                return Response(
-                    status=status.HTTP_201_CREATED
-                )
-            except Exception as e:
-                return Response(
-                    str(e),
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
-            finally:
-                psql_cursor.close()
-                psql_conn.close()
-
-
-class ShowTableView(APIView):
-    """
-    Handles 'SHOW TABLE' views via API.
-    """
-    def get(self, request, *args, **kwargs):
-        """
-        Handles the HTTP GET request.
-        """
-        # TODO: Implement.
-        pass
-
-
-class ImportDataIntoTableView(APIView):
-    """
-    Handles 'COPY INTO' views via API.
-    """
-    def post(self, request, *args, **kwargs):
-        """
-        Handles the HTTP POST request.
-        """
-        file_name = request.data['file']
-        table_name = request.data['table']
-
-        # TODO: Change this route by updating the path where the file is
-        # retrieved from based on the underlying user, which involves a user
-        # model change and migration.
-        #
-        # TODO: Remove hardcoded nature of 'file'
-        file_path = os.path.join(
-            models.TABLE_ROOT,
-            file_name
-        )
-
-        if not utils.import_data_into_table_request_is_valid(
-            file_path,
-            table_name
-        ):
-            return Response(
-                # TODO: Add better error message on validation details.
-                # TODO: Move validation logic from utility method to internal
-                # method, since it's only relevant for a specific API endpoint.
-                'Invalid request',
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        utils.copy_data_into_table(
-            file_path,
-            table_name
-        )
-
-        return Response(
-            'Table is populated with data.',
-            status=status.HTTP_200_OK
-        )
-
-
-"""
-Concrete data service custom views.
-"""
-
-from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from . import models
 from . import serializers
+from core import utils as core_utils
 
 
-class FileUploadView(APIView):
+class CreateTableView(APIView):
     """
-    Upload files via API.
+    Handles 'CREATE FOREIGN TABLE' views via API.
     """
     parser_classes = (
         MultiPartParser,
@@ -173,15 +36,61 @@ class FileUploadView(APIView):
             --header "Content-Type: multipart/form-data" \
             --header "Authorization: JWT $JWT_ACCESS_TOKEN" \
             --method POST \
-            -F file=@sample.csv \
-            https://api.tinydevcrm.com/v1/data/upload/
+            -F file=@sample.parquet \
+            -F table_name=sample_table \
+            -F columns='[{"column_name": "SomeNumber", "column_type":"int"},{"column_name":"SomeString","column_type":"varchar(256)"}]' \
+            https://api.tinydevcrm.com/v1/tables/create/
 
         NOTE: These keys, such as 'data' and 'file', are very particular to the
         underlying models and serializers. Do not change without testing in
         development.
         """
+        def _validate(request):
+            """
+            Validates request data.
 
-        file_serializer = serializers.FileSerializer(
+            Args:
+                rest_framework.request.Request
+
+            Returns:
+                bool: Request is valid.
+            """
+            checks = {
+                'all_required_keys_are_present': True,
+                'column_schema_is_valid': True
+            }
+
+            if (
+                not request.data.get('file') or
+                not request.data.get('table_name') or
+                not request.data.get('columns')
+            ):
+                checks['all_required_keys_are_present'] = False
+
+            columns = request.data.get('columns')
+            try:
+                column_data = json.loads(columns)
+                assert type(column_data) is list
+                for item in column_data:
+                    assert type(item) is dict
+                    assert sorted(item.keys()) == ['column_name', 'column_type']
+                    # TODO: Add check for column types and column names
+            except (Exception, AssertionError) as e:
+                checks['column_schema_is_valid'] = False
+
+            return (
+                all(checks.values()),
+                checks
+            )
+
+        (is_valid, validation_checks) = _validate(request)
+        if not is_valid:
+            return Response(
+                f'Request is not valid: {str(validation_checks)}',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        file_serializer = serializers.DataFileSerializer(
             # Use the form key 'file=' in order to send binary files as part of
             # a multipart/form-data request.
             #
@@ -193,14 +102,56 @@ class FileUploadView(APIView):
                 'file': request.data['file']
             }
         )
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(
-                file_serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        else:
+
+        if not file_serializer.is_valid():
             return Response(
                 file_serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+        file_serializer.save()
+
+        table_name = request.data.get('table_name')
+        columns = json.loads(request.data.get('columns'))
+        column_query = []
+        for column in columns:
+            column_query.append(
+                ' '.join([
+                    '"' + column.get('column_name') + '"',
+                    column.get('column_type').upper()
+                ])
+            )
+        column_query = ', '.join(column_query)
+        column_query = '(' + column_query + ')'
+
+        file_abspath = os.path.join(
+            models.TABLE_ROOT,
+            request.data.get('file').name
+        )
+        sql_query = f'CREATE FOREIGN TABLE "{table_name}" {column_query} SERVER parquet_srv OPTIONS (filename \'{file_abspath}\');'
+
+        # NOTE: Wrap connection in try/except/finally block in order to
+        # responsibly handle possible errors in executing SQL query.
+        try:
+            psql_conn = core_utils.create_fresh_psql_connection()
+            psql_cursor = psql_conn.cursor()
+            psql_cursor.execute(
+                sql.SQL(sql_query)
+            )
+            psql_conn.commit()
+            return Response(
+                status=status.HTTP_201_CREATED
+            )
+        except Exception as e:
+            return Response(
+                str(e),
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        finally:
+            psql_cursor.close()
+            psql_conn.close()
+
+        return Response(
+            file_serializer.data,
+            status=status.HTTP_201_CREATED
+        )
