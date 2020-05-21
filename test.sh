8173236b923efe22f0d2e61f4f81bb62e5982c05
Ying Wang <yingw787.personal@gmail.com>
2020-05-21 12:39:29 -0400

    Update test.sh.

--- test.sh
+++ test.sh
@@ -14,22 +14,17 @@ ACCESS=$(echo $RESPONSE | jq -r ".access")
 echo "Refresh is: " $REFRESH
 echo "Access is: " $ACCESS
 
-# Dry run create a PostgreSQL table (get the schema)
-#
-# NOTE: Dry run should be baked into form / confirmation:
-curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"name": "some_table", "dry_run": "1", "columns": [{"name": "columnA", "type": "varchar(256)"}, {"name": "columnB", "type": "bytea"}]}' http://localhost:8000/v1/tables/create/
-
-# Create a PostgreSQL table.
-curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"name": "some_table", "dry_run": "0", "columns": [{"name": "columnA", "type": "varchar(256)"}, {"name": "columnB", "type": "bytea"}]}' http://localhost:8000/v1/tables/create/
-
-# # Upload a CSV file to remote.
-# curl --header "Content-Type: multipart/form-data" --header "Authorization: JWT $ACCESS" -X POST  -F file=@sample.csv http://localhost:8000/v1/data/upload/
+# Upload a Parquet file to server to create a PostgreSQL foreign table.
+curl --header "Content-Type: multipart/form-data" --header "Authorization: JWT $ACCESS" -X POST  -F file=@sample.parquet -F table_name=sample_table -F columns='[{"column_name": "SomeNumber", "column_type":"int"},{"column_name":"SomeString","column_type":"varchar(256)"}]' http://localhost:8000/v1/tables/create/
 
 # Create materialized view.
 
-# Refresh materialized view.
+# Create scheduled job.
 
-# Create materialized view refreshes table to store job scheduler events.
+# Create materialized view refreshes table to store job scheduler events. Should
+# be done as part of a migration.
+
+# Refresh materialized view.
 
 # Register materialized view with 'pg_cron' and insert materialized view into
 # refreshes table (good to separate out whether or not a refresh is active)
