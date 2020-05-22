1f596623932d7c8d224c9b6a935da4e3a6c32950
Ying Wang <yingw787.personal@gmail.com>
2020-05-22 13:20:04 -0400

    Update views/models.py adding charField instead of specialized enum field.

--- services/app/src/views/models.py
+++ services/app/src/views/models.py
@@ -11,7 +11,19 @@ class EventRefreshes(models.Model):
     trigger definitions for sending out events depend on listening to this table
     specifically for sending out a new event.
     """
+    NEW = 'NEW'
+    SENT = 'SENT'
+    EnumStatusTypes = [
+        (NEW, 'Event not processed'),
+        (SENT, 'Event already processed / sent')
+    ]
+
     # TODO: Change materialized view name from a plain text file to a reference
     # to a materialized views table for when it becomes user-aware.
     materialized_view_name = models.CharField(max_length=256)
     created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=16,
+        choices=EnumStatusTypes,
+        default=NEW
+    )
