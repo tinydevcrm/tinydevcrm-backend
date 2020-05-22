19bb3fc7a11605d04c2bcdf57cf54f8d8019a2f2
Ying Wang <yingw787.personal@gmail.com>
2020-05-22 13:25:02 -0400

    Update views/models.py and add migrations.

--- services/app/src/views/models.py
+++ services/app/src/views/models.py
@@ -3,6 +3,7 @@ Django models for views service.
 """
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class EventRefreshes(models.Model):
@@ -11,19 +12,19 @@ class EventRefreshes(models.Model):
     trigger definitions for sending out events depend on listening to this table
     specifically for sending out a new event.
     """
-    NEW = 'NEW'
-    SENT = 'SENT'
-    EnumStatusTypes = [
-        (NEW, 'Event not processed'),
-        (SENT, 'Event already processed / sent')
-    ]
+    class EnumStatusTypes(models.TextChoices):
+        NEW = 'NEW',_('New event, not processed / sent')
+        SENT = 'SENT',_('Processed event already sent')
 
     # TODO: Change materialized view name from a plain text file to a reference
     # to a materialized views table for when it becomes user-aware.
+    # TODO: Change status from a char field to a custom model field for actual
+    # postgresql enum types, potentially adding this to Django middleware:
+    # https://schinckel.net/2019/09/18/postgres-enum-types-in-django/
     materialized_view_name = models.CharField(max_length=256)
     created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         max_length=16,
-        choices=EnumStatusTypes,
-        default=NEW
+        choices=EnumStatusTypes.choices,
+        default=EnumStatusTypes.NEW
     )
