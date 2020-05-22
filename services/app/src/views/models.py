60430c1efce451fb5872f98b12b7dd14d4c1bfda
Ying Wang <yingw787.personal@gmail.com>
2020-05-22 14:01:24 -0400

    Update views/models.py

--- services/app/src/views/models.py
+++ services/app/src/views/models.py
@@ -6,16 +6,17 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class EnumStatusTypes(models.TextChoices):
+    NEW = 'NEW',_('New event, not processed / sent')
+    SENT = 'SENT',_('Processed event already sent')
+
+
 class EventRefreshes(models.Model):
     """
     Models for issuing materialized view refreshes. This is important because
     trigger definitions for sending out events depend on listening to this table
     specifically for sending out a new event.
     """
-    class EnumStatusTypes(models.TextChoices):
-        NEW = 'NEW',_('New event, not processed / sent')
-        SENT = 'SENT',_('Processed event already sent')
-
     # TODO: Change materialized view name from a plain text file to a reference
     # to a materialized views table for when it becomes user-aware.
     # TODO: Change status from a char field to a custom model field for actual
