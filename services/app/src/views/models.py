92a12afdcc757d62b981bf68372e2b37b6482a39
Ying Wang <yingw787.personal@gmail.com>
2020-05-22 12:59:45 -0400

    Update views/models.py adding model for event refreshes.

--- services/app/src/views/models.py
+++ services/app/src/views/models.py
@@ -3,3 +3,15 @@ Django models for views service.
 """
 
 from django.db import models
+
+
+class EventRefreshes(models.Model):
+    """
+    Models for issuing materialized view refreshes. This is important because
+    trigger definitions for sending out events depend on listening to this table
+    specifically for sending out a new event.
+    """
+    # TODO: Change materialized view name from a plain text file to a reference
+    # to a materialized views table for when it becomes user-aware.
+    materialized_view_name = models.CharField(max_length=256)
+    created = models.DateTimeField(auto_now_add=True)
