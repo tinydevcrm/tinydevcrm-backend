520924b161635c064ed3dea8def209d07ab800cf
Ying Wang <yingw787.personal@gmail.com>
2020-05-25 13:23:43 -0400

    Update test.sh make materialized view user aware

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ echo "Access is: " $ACCESS
 curl --header "Content-Type: multipart/form-data" --header "Authorization: JWT $ACCESS" -X POST  -F file=@sample.parquet -F table_name=sample_table -F columns='[{"column_name": "SomeNumber", "column_type":"int"},{"column_name":"SomeString","column_type":"varchar(256)"}]' http://localhost:8000/tables/create/
 
 # Create materialized view.
-curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"view_name": "sample_view", "sql_query": "SELECT * FROM \"1\".\"sample_table\""}' http://localhost:8000/views/create/
+curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"view_name": "\"1\".\"sample_view\"", "sql_query": "SELECT * FROM \"1\".\"sample_table\""}' http://localhost:8000/views/create/
 
 # Create scheduled job.
 curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"view_name": "sample_view", "crontab_def": "* * * * *"}' http://localhost:8000/jobs/create/
