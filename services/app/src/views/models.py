6d46b2429db57d91a4d2321d464cbd462cc2ce80
Ying Wang <yingw787.personal@gmail.com>
2020-05-25 13:17:10 -0400

    Update views/models.py add MaterializedView model.

--- services/app/src/views/models.py
+++ services/app/src/views/models.py
@@ -5,6 +5,8 @@ Django models for views service.
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from authentication import models as auth_models
+
 
 class EnumStatusTypes(models.TextChoices):
     NEW = 'NEW',_('New event, not processed / sent')
@@ -28,4 +30,23 @@ class EventRefreshes(models.Model):
         max_length=16,
         choices=EnumStatusTypes.choices,
         default=EnumStatusTypes.NEW
+    )
+
+
+class MaterializedView(models.Model):
+    """
+    Django model for materialized view. This model is important for describing
+    and managing jobs and channels, as those models have a one-to-one
+    relationship with an underlying view.
+
+    TODO: Describe tables with foreign keys and come up with a proper foreign
+    key deletion strategy. Right now, since materialized views are described
+    with an unstructured query, this isn't possible, though leverage of
+    'sql_parse' libraries like 'pglast' and 'psqlparse' may assist things.
+    """
+    view_name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        auth_models.CustomUser,
+        on_delete=models.PROTECT,
+        to_field='id'
     )
