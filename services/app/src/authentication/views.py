dc6ff10c44bf72491970511b00ba877520023d9d
Ying Wang <yingw787.personal@gmail.com>
2020-05-25 16:10:23 -0400

    Update authentication/views.py create schema if not exists during user registration.

--- services/app/src/authentication/views.py
+++ services/app/src/authentication/views.py
@@ -3,12 +3,14 @@ Authentication service custom views.
 """
 
 from django.shortcuts import render
+from psycopg2 import sql
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from core import utils as core_utils
 from .serializers import CustomTokenObtainPairSerializer, CustomUserSerializer
 
 
@@ -28,6 +30,18 @@ class CustomUserRegister(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
+                # If the user is successfully created and saved into the
+                # database, create a schema for that user in order to place all
+                # subsequent database resources.
+                with core_utils.PostgreSQLCursor() as (psql_conn, psql_cursor):
+                    create_schema_sql_statement = sql.SQL(
+                        "CREATE SCHEMA IF NOT EXISTS {}"
+                    ).format(
+                        sql.Identifier(str(user.id))
+                    )
+                    psql_cursor.execute(create_schema_sql_statement)
+                    psql_conn.commit()
+
                 json = serializer.data
                 return Response(
                     json,
