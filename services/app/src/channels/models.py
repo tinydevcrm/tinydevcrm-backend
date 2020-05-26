f8b8361970efcaa06bf92339d42b04f2865d0d8a
Ying Wang <yingw787.personal@gmail.com>
2020-05-26 13:41:52 -0400

    Update channels/models.py

--- services/app/src/channels/models.py
+++ services/app/src/channels/models.py
@@ -1,3 +1,37 @@
+"""
+Django models for channels service.
+"""
+
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from authentication import models as auth_models
+from jobs import models as jobs_models
+from views import models as views_models
+
+
+class Channel(models.Model):
+    """
+    Model for managing channels.
+    """
+    # Public identifier of channel, used in URL params.
+    public_identifier = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False
+    )
+    job = models.ForeignKey(
+        jobs_models.CronJob,
+        on_delete=models.PROTECT,
+        to_field='job_id'
+    )
+    user = models.ForeignKey(
+        auth_models.CustomUser,
+        on_delete=models.PROTECT,
+        to_field='id'
+    )
+    view = models.ForeignKey(
+        views_models.MaterializedView,
+        on_delete=models.PROTECT,
+        to_field='id'
+    )
