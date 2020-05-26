a78c7ff64073108e10cfc3e6853231e305037843
Ying Wang <yingw787.personal@gmail.com>
2020-05-25 23:39:35 -0400

    Update test.sh.

--- test.sh
+++ test.sh
@@ -18,17 +18,17 @@ echo "Access is: " $ACCESS
 curl --header "Content-Type: multipart/form-data" --header "Authorization: JWT $ACCESS" -X POST  -F file=@sample.parquet -F table_name=sample_table -F columns='[{"column_name": "SomeNumber", "column_type":"int"},{"column_name":"SomeString","column_type":"varchar(256)"}]' http://localhost:8000/tables/create/
 
 # Create materialized view.
-curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"view_name": "\"1\".\"sample_view\"", "sql_query": "SELECT * FROM \"1\".\"sample_table\""}' http://localhost:8000/views/create/
+curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"view_name": "sample_view", "sql_query": "SELECT * FROM sample_table"}' http://localhost:8000/views/create/
 
 # Create scheduled job.
 curl --header "Content-Type: application/json" --header "Authorization: JWT $ACCESS" -X POST --data '{"view_name": "sample_view", "crontab_def": "* * * * *"}' http://localhost:8000/jobs/create/
 
 # Create materialized view refreshes table to store job scheduler events. Should
-# be done as part of a migration.
+# be done as part of a migration. DONE
 
-# Refresh materialized view.
+# Refresh materialized view. DONE
 
 # Register materialized view with 'pg_cron' and insert materialized view into
-# refreshes table (good to separate out whether or not a refresh is active)
+# refreshes table (good to separate out whether or not a refresh is active) DONE
 
 # Issue event on pub/sub upon insert into_table refresh view event.
