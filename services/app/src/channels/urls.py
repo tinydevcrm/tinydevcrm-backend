e76d7cc851f59c12733b2a8e4ba813671593cd8a
Ying Wang <yingw787.personal@gmail.com>
2020-05-27 15:33:51 -0400

    Update channels/urls.py with stub urlpatterns, might need to change after adding pushpin.

--- services/app/src/channels/urls.py
+++ services/app/src/channels/urls.py
@@ -12,5 +12,20 @@ urlpatterns = [
         'create/',
         views.CreateChannelView().as_view(),
         name='channel_create'
+    ),
+    path(
+        '<uuid:identifier>/open/',
+        views.OpenChannelView().as_view(),
+        name='channel_open'
+    ),
+    path(
+        '<uuid:identifier>/close/',
+        views.CloseChannelView().as_view(),
+        name='channel_close'
+    ),
+    path(
+        '<uuid:identifier>/listen/',
+        views.ListenChannelView().as_view(),
+        name='channel_listen'
     )
 ]
