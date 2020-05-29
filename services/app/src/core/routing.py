df67335a07d48582d5a7e060e243aa0102ebf004
Ying Wang <yingw787.personal@gmail.com>
2020-05-29 15:05:02 -0400

    Update core/routing.py.

--- services/app/src/core/routing.py
+++ services/app/src/core/routing.py
@@ -1,16 +1,22 @@
 """
 Routing logic for ASGI server.
 
-If there are additional protocols that need to be supported besides HTTP, add
-them within this file.
+NOTE: If there are additional protocols that need to be supported besides HTTP,
+add them within this file.
+
+NOTE: It looks like the regular urls.py and urlpatterns syntax will work
+successfully for 'http' endpoints, and that nesting URLRouter() objects for
+multiple different Django applications isn't necessary. Therefore, only create
+'routing.py' references and import only the Django applications that require
+explicit ASGI/aynchronous support.
 """
 
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
 
-from . import urls
+from channels_app import routing as channels_app_routing
 
 
 application = ProtocolTypeRouter({
-    'http': URLRouter(urls.urlpatterns)
+    'http': URLRouter(channels_app_routing.urlpatterns)
 })
