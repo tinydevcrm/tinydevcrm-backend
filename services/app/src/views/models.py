75f69564937917694cc129b236dd806a472f3ab4
Ying Wang <yingw787.personal@gmail.com>
2020-06-02 14:33:45 -0400

    Update views/models.py remove eventrefreshes reference.

--- services/app/src/views/models.py
+++ services/app/src/views/models.py
@@ -3,34 +3,8 @@ Django models for views service.
 """
 
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 from authentication import models as auth_models
-
-
-class EnumStatusTypes(models.TextChoices):
-    NEW = 'NEW',_('New event, not processed / sent')
-    SENT = 'SENT',_('Processed event already sent')
-
-
-class EventRefreshes(models.Model):
-    """
-    Models for issuing materialized view refreshes. This is important because
-    trigger definitions for sending out events on a channel depend on listening
-    to this table specifically for sending out a new event.
-    """
-    # TODO: Change materialized view name from a plain text file to a reference
-    # to a materialized views table for when it becomes user-aware.
-    # TODO: Change status from a char field to a custom model field for actual
-    # postgresql enum types, potentially adding this to Django middleware:
-    # https://schinckel.net/2019/09/18/postgres-enum-types-in-django/
-    materialized_view_name = models.CharField(max_length=256)
-    created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(
-        max_length=16,
-        choices=EnumStatusTypes.choices,
-        default=EnumStatusTypes.NEW
-    )
 
 
 class MaterializedView(models.Model):
