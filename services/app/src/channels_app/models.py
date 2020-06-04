914ee2bc0cdb18dd49bb5f81e60d910dd4fef2ad
Ying Wang <yingw787.personal@gmail.com>
2020-06-04 12:50:59 -0400

    Update channels_app/models.py removing extraneous fields and adding copious TODOs.

--- services/app/src/channels_app/models.py
+++ services/app/src/channels_app/models.py
@@ -14,36 +14,50 @@ from authentication import models as auth_models
 from jobs import models as jobs_models
 
 
-CHANNELS_URL = 'channels'
-CHANNELS_ROOT = os.path.join(
-    settings.MEDIA_ROOT,
-    CHANNELS_URL
-)
-
-
-class EnumChannelStatusTypes(models.TextChoices):
-    ACTIVE = 'ACTIVE',_('Channel is open and active, currently sending events out')
-    INACTIVE = 'INACTIVE',_('Channel has been created, but is not active')
-
-
-def default_channels_json_payload():
-    """
-    Wraps the default JSON payload for the Django channels model in order to
-    enforce immutability.
-    """
-    return {
-        'update_available': 'true'
-    }
-
-
 class Channel(models.Model):
     """
     Model for managing channels.
+
+    TODO: Somehow integrate active / inactive channel status into the '/listen/'
+    API endpoint. Right now, 'listen/' only uses 'django_eventstream' URLs and
+    does not tie into the Django ORM models, so each channel does not have
+    awareness of whether it is active or not. Therefore, any valid string can be
+    a channel to be listened to, instead of only the ones that are declared.
+
+    Originally, I had this snippet of code as part of my Django model:
+
+    class EnumChannelStatusTypes(models.TextChoices):
+        ACTIVE = 'ACTIVE',_('Channel is open and active, currently sending events out')
+        INACTIVE = 'INACTIVE',_('Channel has been created, but is not active')
+
+    channel_status = models.CharField(
+        max_length=16,
+        choices=EnumChannelStatusTypes.choices,
+        default=EnumChannelStatusTypes.INACTIVE
+    )
+
+    TODO: Maybe have the ability to send over JSONified table data, limited to
+    some number of records. This was the original intent of having a
+    configurable JSON payload. Otherwise, it will always be the same JSON
+    payload, which will require another network call by the client in order to
+    get the required information.
+
+    Originally, I had this snippet of code as part of the Django model:
+
+    def default_channels_json_payload():
+        return {
+            'update_available': 'true'
+        }
+
+    json_payload = psql_fields.JSONField(
+        default=default_channels_json_payload
+    )
     """
     # Public identifier of channel, used in URL params.
     public_identifier = models.UUIDField(
         default=uuid.uuid4,
-        editable=False
+        editable=False,
+        unique=True
     )
     # TODO: Currently there's a many-to-one correlation between channels and
     # jobs. As in, many channels can reference one job. I think this is
@@ -64,16 +78,4 @@ class Channel(models.Model):
         auth_models.CustomUser,
         on_delete=models.PROTECT,
         to_field='id'
-    )
-    storedprocedure_file = models.FileField(
-        null=True,
-        upload_to=CHANNELS_URL
-    )
-    channel_status = models.CharField(
-        max_length=16,
-        choices=EnumChannelStatusTypes.choices,
-        default=EnumChannelStatusTypes.INACTIVE
-    )
-    json_payload = psql_fields.JSONField(
-        default=default_channels_json_payload
     )
