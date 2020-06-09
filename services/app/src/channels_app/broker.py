e6213cb1bab2eb7267a46b504ef247366fb4c123
Ying Wang <yingw787.personal@gmail.com>
2020-06-09 18:42:55 -0400

    Update broker.py changing print() statements to logging.info() statements.

--- services/app/src/channels_app/broker.py
+++ services/app/src/channels_app/broker.py
@@ -11,6 +11,7 @@ import django_eventstream
 import psycopg2
 from psycopg2 import sql
 
+from core import app_logging
 from core import utils as core_utils
 from views import models as view_models
 from channels_app import models
@@ -29,11 +30,7 @@ def broker_proc():
     the database, and create per-trigger channels, and scale out the database
     and reverse proxy as needed.
     """
-    # NOTE: These models aren't imported at the top level because during
-    # startup, the models do not yet exist. Otherwise, you will get a
-    # 'django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet'.
-    # Therefore, only load models during function call.
-    # method call.
+    logger = app_logging.get_broker_logger()
 
     CHANNEL_NAME = 'psql_refreshes_channel'
 
@@ -51,15 +48,14 @@ def broker_proc():
             # If Linux select() syscall returns empty, then do not poll for
             # updates using 'psycopg2'.
             if select.select([psql_conn],[],[],5) == ([],[],[]):
-                # NOTE: 'logging.info' does not print to stdout by default
-                # here, using print() for convenience.
-                print("No information from handler yet.")
+                logger.info("No information from handler yet.")
             else:
                 psql_conn.poll()
                 while psql_conn.notifies:
-                    # NOTE: This should be fine...there should be no need
-                    # for something like 'multiprocessing', the load
-                    # shouldn't be there.
+                    # NOTE: Processing one event at a time should be
+                    # fine...there should be no need for something like
+                    # 'multiprocessing', the load shouldn't be there. Update if
+                    # different in production.
                     #
                     # NOTE: Need to pop off psql_conn.notifies to avoid
                     # infinite loop while psql_conn.notifies exists. This
@@ -69,18 +65,21 @@ def broker_proc():
                     job_id = payload['job_id']
                     view_id = payload['view_id']
 
-                    print("Payload: ", payload)
+                    logger.info("Payload: ", payload)
 
                     view_name = view_models.MaterializedView.objects.filter(id=view_id)[0].view_name
 
-                    print('Materialized view name: ', view_name)
+                    logger.info('Materialized view name: ', view_name)
 
                     channels = models.Channel.objects.filter(
                         job_id=job_id
                     )
 
                     for channel in channels:
-                        print('Sending update to channel UUID: ', channel.public_identifier)
+                        logger.info(
+                            'Sending update to channel UUID: ',
+                            channel.public_identifier
+                        )
                         django_eventstream.send_event(
                             str(channel.public_identifier),
                             'message',
